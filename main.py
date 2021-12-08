bb59c48bc2921d9ff362718aba1c0c8ec2cdc436
Temidola <haryodollybim@gmail.com>
2021-12-08 00:34:31 -0500

    Add Table View

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from view import Start, DataEntry, DataNorg
 
-
-
-
 if __name__ == "__main__":
     app = DataNorg()
     app.run()
-
-
-
 
 
--- view.py
+++ view.py
@@ -2,21 +2,56 @@ from dataclasses import dataclass
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.uix.image import Image
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.popup import Popup
 
+from typing import Text
+
+
+
+MIN_AMOUNT_OF_TABLES = 1
+MAX_AMOUNT_OF_TABLES = 256
 
 class Start(Screen):
+    _popup: object
+    
     def __init__(self, **kwargs):
         super(Start, self).__init__(**kwargs)
+        self._popup = PopupWarningMessage()
+        
+    def switch_screen_to_dataentry(self, *args) -> None:
+        if self.is_correct_data():
+            app = App.get_running_app()
+            app.root.current = "dataentry"
+        else:
+            self.open_warning_message()
+
+    def is_correct_data(self) -> bool:
+        str_value = self.ids.number_of_tables.text
+        try:
+            value = int(str_value)
+        except:
+            return False
+        return MIN_AMOUNT_OF_TABLES <= int(str_value) <= MAX_AMOUNT_OF_TABLES
+
+    def open_warning_message(self) -> object:
+        self._popup.open()
+        
+        
+        
+        
 
 class DataEntry(Screen):
     def __init__(self, **kwargs):
         super(DataEntry, self).__init__(**kwargs)
 
+
+
+class PopupWarningMessage(Popup):
+   pass 
+
 class DataNorg(App):
-    def build(self):
+    def build(self) -> object:
         self.icon = 'datanorg.png'
         screen_manager = ScreenManager()
         start_screen = Start(name="start")
